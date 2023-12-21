aae51a4ee41899163da4c18011955091d45d2094
MUHAMMADAZAN1 <144342317+MUHAMMADAZAN1@users.noreply.github.com>
2023-12-21 23:11:34 +0500

    the function maded

---
+++ airDistanceTester.c
@@ -0,0 +1,27 @@
+// use to run the different source file
+#include<math.h>
+#include<stdio.h>
+// add the utils.h header to use our function which present in another source file or object file
+#include "utils.h"
+int main(){
+    double airDistance;
+ double originLatitude,originLongitude;
+ double destinationLatitude,destinationLongitude;
+ printf("Enter the Latitude and Longitude of Origin(latitude,longitude): ");
+ scanf("%lf,%lf",&originLatitude,&originLongitude);
+ printf("Enter the Latitude and Longitude of Destination(latitude,longitude): ");
+ scanf("%lf,%lf",&destinationLatitude,&destinationLongitude);
+ airDistance = getAirDistance(originLatitude,originLongitude,destinationLatitude,destinationLongitude);
+ printf("Air Distance  = %0.2lf\n Km",airDistance);
+ printf("\n");
+ // take input from user 
+ // i made flexible for user to give the value by itself
+ double percentVelocity, time;
+ printf("Enter the time observed it rest in year: ");
+ scanf("%lf",&time);
+ printf("Enter the Velocity of the object scale[0,1]: ");
+ scanf("%lf",&percentVelocity);
+ lorentzTimeDilation(time, percentVelocity);
+ printf("the time dilation = %0.2lf year\n",lorentzTimeDilation(time, percentVelocity));
+
+}
