07aa45559ebdeaa4a03fa05efce430fdf51a535b
MUHAMMADAZAN1 <144342317+MUHAMMADAZAN1@users.noreply.github.com>
2023-12-22 00:10:28 +0500

    function returns distance value

--- airDistance.c
+++ airDistance.c
@@ -4,15 +4,4 @@
 // i write a function which return distance value
 double getAirDistance(double originLatitude,double originLongitude,
                       double destinationLatitude,double destinationLongitude){
-    // use self maded degreesToRadians Function convert degrees to radian which present in object file                   
-    originLatitude = degreesToRadians(originLatitude);
-    originLongitude = degreesToRadians(originLongitude);
-    destinationLatitude = degreesToRadians(destinationLatitude);
-    destinationLongitude = degreesToRadians(destinationLongitude);
-    double delta = destinationLongitude - originLongitude;
-    float const R = 6371;
-    // spherical law
-    double distance = acos(sin(originLatitude) * sin(destinationLatitude) + cos(originLatitude) * cos(destinationLatitude) *cos(delta)) * R;
-    // return distance
-    return distance;
-}
+   
